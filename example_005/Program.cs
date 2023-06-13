dbe0c30bf7262288a9a98d867da33ed65df1291d
Andrey <atishin1999@mail.ru>
2023-06-13 15:50:47 +0300

    examle 5 имена

---
+++ example_005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "андрей")
+{
+    Console.WriteLine("Урааааа, это Андрей !!!!!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
